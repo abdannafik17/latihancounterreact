cae3a0f7e1ca3a2c034c336fb9dff206ad3117bd
Abdan Nafik <abdan.nafik@pln.co.id>
2019-04-26 06:56:37 +0700

    aplikasi counter reactjs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//stateless component
+
+import React, { Component } from 'react'
+import './App.css'
+import Board from './components/Board'
+import Plus from './components/Plus'
+import Minus from './components/Minus';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      score : 0,
+      text : 'Halo'
+    }
+  }
+
+  plusOne () {
+    let { score } = this.state //destructuring
+    this.setState({
+      score: ++score
+    })
+  }
+
+  minusOne = () => {
+    let { score } = this.state //destructuring
+    this.setState({
+      score: --score
+    })
+  }
+
+  updateText (params) {
+    this.setState({
+      text: params
+    })
+  }
+  //atau
+  updateText1 = (element) => {
+    this.setState({
+      text: element.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Board 
+          score = { this.state.score }
+        />
+        <div className="controller">
+          <Minus substract={this.minusOne} />
+
+          <Plus add={this.plusOne.bind(this)}/>          
+        </div>
+        <h1>{this.state.text}</h1>
+        <input type="text"
+          placeholder={this.state.text}
+          /*onChange={(element) => {
+            this.updateText(element.target.value)
+            //element dan element.target.value adalah syntax basic dari ajax untuk mendapatkan value dari dirinya sendiri
+          }}*/ 
+          //atau menggunakan bind
+          onChange = {this.updateText1.bind(this)}
+        />
       </div>
-    );
+    )
   }
 }
 
 export default App;
+
+/** Catatan :
+ * Store -> View -> Action
+ * App.js -> Board.js, Plus.js, Minus.js -> Button OnClick
+ */
